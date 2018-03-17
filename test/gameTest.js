4f26dbbbe381d431527bb5a5014ceac833506e1a
Tobias <tobias.baden@t-online.de>
2018-03-18 00:17:51 +0100

    Test for the "dealACardToThePlayer" function, but they dont work
    Some problems with the module.exports i guess. Fixed soon before continouing

--- test/gameTest.js
+++ test/gameTest.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
-const game = require('../src/game');
+const game = require('../src/game').init;
 
 
 describe('Game', function () {
@@ -8,7 +8,7 @@ describe('Game', function () {
     var testgame = null;
 
     beforeEach(function () {
-        testgame = new game.init();
+        testgame = new game;
     });
 
     describe('after initialisation', function () {
@@ -23,9 +23,29 @@ describe('Game', function () {
         it('should generate a complete deck of 52 cards', function () {
             expect(testgame.playDeck.length, 52);
         });
-        
+
         it('should be an score of zero after calculating it', function () {
             expect(testgame.calculateScoreForTheHand, 0)
         });
     });
+
+    describe('if a new card was requested', function () {
+        it('should deal a new card to the player', function () {
+            var playerHandLengthBefore = testgame.playerHand.length;
+            //FIXME is not a function problem - module.exports error
+            testgame.dealACardToThePlayer();
+            expect(testgame.playerHand.length, playerHandLengthBefore+1);
+        });
+
+        it('should calculate the new score for the hand automaticly and correct', function () {
+            //FIXME is not a function problem - module.exports error
+            testgame.dealACardToThePlayer();
+            expect(testgame.calculateScoreForTheHand, 1);
+        });
+
+        it('should only deal one new card', function () {
+            //FIXME is not a function problem - module.exports error
+            expect(testgame.dealACardToThePlayer(5), 1)
+        });
+    })
 });
