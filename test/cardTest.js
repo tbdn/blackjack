d8946c6ea6e558348b4bbd196fb2017cd90a7bbd
Tobias Baden <tobias.baden@t-online.de>
2018-03-17 23:04:46 +0100

    Created cards.js for the cards an added some tests

---
+++ test/cardTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+const cards = require('../src/cards');
+
+describe('Cards', function () {
+    it('Card deck should be an array', function () {
+        expect(cards.deck).to.be.an('array');
+    });
+
+    it('Card deck should contain 52 cards', function () {
+        assert.equal(cards.deck.length, 52);
+    });
+});
